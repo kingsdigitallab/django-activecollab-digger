f55640242366a8a0fddfb944d0927e978283ad37
Miguel Vieira <jmvieira@gmail.com>
2017-06-19 15:03:42 +0100

    Increased version.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='django-activecollab-digger',
 
-    version='0.2',
+    version='0.3',
 
     description='Django ActiveCollab application',
     long_description=long_description,
